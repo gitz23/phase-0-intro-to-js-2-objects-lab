c9b25bae31d4fa6c308f2b0858f284a6d5556883
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-12 12:19:05 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee ={
+    name: "Sam",
+    streetAddress: "12 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, Sam){
+    const newName ={...employee};
+    newName[name] = "11 Broadway";
+    return newName;
+}
+
+function deleteFromEmployeeByKey(employee, name){
+    let newEmployee = {...employee}
+    delete newEmployee[name];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+    return employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, Broadway){
+    delete employee[streetAddress];
+    return employee;
+}
